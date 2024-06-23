48ce27d5eb972dade6593a39e8084295c6a46398
Koustubh <44812255+Koustubhumdekar@users.noreply.github.com>
2024-06-23 14:10:52 +0530

    Update GatewayController.java

--- api-gateway/src/main/java/com/example/api_gateway/controller/GatewayController.java
+++ api-gateway/src/main/java/com/example/api_gateway/controller/GatewayController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Koustubh Umdekar
+ *
+ */
 
 @RestController
 @RequestMapping("/gateway")
