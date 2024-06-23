e5457910ccaf8123a90210a205d7db0957a90c8d
Koustubh <44812255+Koustubhumdekar@users.noreply.github.com>
2024-06-23 13:43:10 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/example/api_gateway/controller/GatewayController.java
@@ -0,0 +1,23 @@
+package com.example.api_gateway.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/gateway")
+public class GatewayController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GatewayController.class);
+
+    @GetMapping("/gateway")
+    public String gatewayEndpoint(){
+        logger.info("gateway endpoint..");
+
+        return "API Gateway is working";
+    }
+}
